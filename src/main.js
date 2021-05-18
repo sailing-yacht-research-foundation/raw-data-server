2530d983829cb484b0a03a842c66ae7c833faf15
AlvinJK <ajk.alvin@gmail.com>
2021-05-18 16:33:03 +0700

    fix kattack test, add docker file

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const createServer = require('./server');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 
 const app = createServer();
 app.listen(port, () => {
