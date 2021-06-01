1db17d8d81dc6e69071150c8c4bb4e5db4e90aae
AlvinJK <ajk.alvin@gmail.com>
2021-06-01 16:35:40 +0700

    Added error handler on writerParquet

--- src/services/writeToParquet.js
+++ src/services/writeToParquet.js
@@ -1,14 +1,23 @@
 const parquet = require('parquetjs-lite');
 
 const writeToParquet = async (data, schema, filePath) => {
-  const writer = await parquet.ParquetWriter.openFile(schema, filePath, {
-    useDataPageV2: false,
-  });
-
-  for (let i = 0; i < data.length; i++) {
-    await writer.appendRow(data[i]);
+  let writer;
+  let i;
+  try {
+    writer = await parquet.ParquetWriter.openFile(schema, filePath, {
+      useDataPageV2: false,
+    });
+    for (i = 0; i < data.length; i++) {
+      await writer.appendRow(data[i]);
+    }
+    await writer.close();
+  } catch (error) {
+    console.error(`Parquet writing fails on data #${i}, error: ${error}`);
+    console.table([data[i]]);
+    if (writer) {
+      writer.close();
+    }
   }
-  await writer.close();
 };
 
 module.exports = writeToParquet;
