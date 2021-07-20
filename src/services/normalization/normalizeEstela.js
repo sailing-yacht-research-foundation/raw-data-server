1a2bb9473a31aa2edc3874aab310ce6fe3763474
Jackson Tan <jackson.tan.s@gmail.com>
2021-07-21 04:16:15 +0800

    SNS-183 - Add Estela Normalization and improve unit test on normalization (#12)
    * SNS-182 - Add utilility functions and models for normalization
    
    * SNS-182 - Add Bluewater normalization code
    
    * SNS-183 - Add Estela Normalization and improve unit test on normalization
    
    Co-authored-by: markomakaric <marko.m@scopicsoftware.com>

---
+++ src/services/normalization/normalizeEstela.js
@@ -0,0 +1,108 @@
+const turf = require('@turf/turf');
+const db = require('../../models');
+const {
+  createBoatToPositionDictionary,
+  positionsToFeatureCollection,
+  collectLastNPositionsFromBoatsToPositions,
+  getCenterOfMassOfPositions,
+  findAverageLength,
+  createRace,
+  createTurfPoint,
+  allPositionsToFeatureCollection,
+} = require('../../utils/gisUtils');
+const { uploadGeoJsonToS3 } = require('../uploadFileToS3');
+
+const normalizeRace = async (
+  { EstelaRace, EstelaPosition, EstelaDorsal, EstelaBuoy },
+  transaction,
+) => {
+  const ESTELA_SOURCE = 'ESTELA';
+  const race = EstelaRace[0];
+  const allPositions = EstelaPosition;
+  const allBuoys = EstelaBuoy;
+  const boats = EstelaDorsal;
+  const id = race.id;
+  const name = race.name;
+  const event = null;
+  const url = race.url;
+  const startTime = new Date(race.start_timestamp * 1000).getTime();
+  const endTime = new Date(race.end_timestamp * 1000).getTime();
+  let startPoint = createTurfPoint(race.initLat, race.initLon);
+
+  if (allPositions.length === 0) {
+    console.log('No positions so skipping.');
+    return;
+  }
+
+  const boundingBox = turf.bbox(
+    positionsToFeatureCollection('lat', 'lon', allPositions),
+  );
+  const boatsToSortedPositions = createBoatToPositionDictionary(
+    allPositions,
+    'dorsal',
+    'timestamp',
+  );
+  const last3Positions = collectLastNPositionsFromBoatsToPositions(
+    boatsToSortedPositions,
+    3,
+  );
+  let endPoint = getCenterOfMassOfPositions('lat', 'lon', last3Positions);
+  const buoys = allBuoys.filter((b) => b.race === race.id);
+  if (buoys.length > 2) {
+    buoys.sort((a, b) => (parseInt(a.index) > parseInt(b.index) ? 1 : -1));
+
+    const startBuoy = buoys[0];
+    const endBuoy = buoys[buoys.length - 1];
+    if (startBuoy.lat !== null && startBuoy.lon !== null) {
+      startPoint = createTurfPoint(startBuoy.lat, startBuoy.lon);
+    }
+    if (endBuoy.lat !== null && endBuoy.lon !== null) {
+      endPoint = createTurfPoint(endBuoy.lat, endBuoy.lon);
+    }
+  }
+  const boatNames = [];
+  const boatModels = [];
+  const handicapRules = [];
+  const boatIdentifiers = [];
+  const unstructuredText = [];
+
+  for (const boatIndex in boats) {
+    const b = boats[boatIndex];
+    boatNames.push(b.name);
+    boatModels.push(b.model);
+    boatIdentifiers.push(b.mmsi);
+    boatIdentifiers.push(b.number);
+  }
+
+  const roughLength = findAverageLength('lat', 'lon', boatsToSortedPositions);
+  const raceMetadata = await createRace(
+    id,
+    name,
+    event,
+    ESTELA_SOURCE,
+    url,
+    startTime,
+    endTime,
+    startPoint,
+    endPoint,
+    boundingBox,
+    roughLength,
+    boatsToSortedPositions,
+    boatNames,
+    boatModels,
+    boatIdentifiers,
+    handicapRules,
+    unstructuredText,
+  );
+  const tracksGeojson = JSON.stringify(
+    allPositionsToFeatureCollection(boatsToSortedPositions),
+  );
+
+  await db.readyAboutRaceMetadata.create(raceMetadata, {
+    fields: Object.keys(raceMetadata),
+    transaction,
+  });
+  await uploadGeoJsonToS3(race.id, tracksGeojson, ESTELA_SOURCE, transaction);
+};
+
+exports.normalizeRace = normalizeRace;
--- src/services/saveBluewaterData.js
+++ src/services/saveBluewaterData.js
@@ -69,12 +69,9 @@ const saveBluewaterData = async (data) => {
       });
     }
     if (data.BluewaterPosition) {
-      const positions = [...data.BluewaterPosition]; // clone array to avoid mutating the data
+      const positions = data.BluewaterPosition.slice(); // clone array to avoid mutating the data
       while (positions.length > 0) {
-        const splicedArray = positions.splice(
-          0,
-          SAVE_DB_POSITION_CHUNK_COUNT,
-        );
+        const splicedArray = positions.splice(0, SAVE_DB_POSITION_CHUNK_COUNT);
         await db.bluewaterPosition.bulkCreate(splicedArray, {
           ignoreDuplicates: true,
           validate: true,
