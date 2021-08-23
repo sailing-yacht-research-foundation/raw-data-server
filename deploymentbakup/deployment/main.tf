e253ebe1f45a5f0b7da1c7ac9f597ba16f5b48fe
femi <femmie009@gmail.com>
2021-08-23 11:18:18 +0300

    change in src/main.js

---
+++ deploymentbakup/deployment/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  backend "s3" {
+    bucket = "syrf-rawdata-terraform-state"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-1"
+
+    dynamodb_table = "terraform-state-locking"
+    encrypt = true
+  }
+}
+# Providing a reference to our default VPC
+resource "aws_default_vpc" "default_vpc" {
+}
+
+# Providing a reference to our default subnets
+resource "aws_default_subnet" "default_subnet" {
+  count             = length(var.aws_availability_zones)
+  availability_zone = var.aws_availability_zones[count.index]
+}
