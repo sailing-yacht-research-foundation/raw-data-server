e17031a2dab721d8052b401f1408dc9397103699
femi <femmie009@gmail.com>
2021-10-01 14:55:57 +0300

    added cicd for ecs and consolidated vars SNS-327

--- deployment/variable.tf
+++ deployment/variable.tf
@@ -13,80 +13,10 @@ variable "aws_availability_zones" {
   description = "Availability zone list"
 }
 
-variable "db_username" {
-  default     = "user"
-  description = "Username for MySQL Database"
-}
-
-variable "db_name" {
-  default     = "rawdata"
-  description = "Database name"
-}
 
 variable "app_container_port" {
   default     = 3000
   description = "Container Port"
 }
 
-variable "s3_access_key_id" {
-  type = string
-}
 
-variable "s3_secret_key" {
-  type = string
-}
-
-variable "s3_bucket" {
-  type    = string
-  default = "raw-data-server-project"
-}
-
-variable "mq_host" {
-  type = string
-
-}
-
-variable "mq_port" {
-  type = string
-}
-
-variable "mq_user" {
-  type = string
-
-}
-
-variable "mq_password" {
-  type = string
-
-}
-
-variable "mq_topic" {
-  default = "/topic/stream.data"
-}
-
-variable "mq_timeout" {
-  default = 2700000
-}
-
-variable "aws_es_host" {
-  default = "335855654610:readyabout-dev"
-  type    = string
-}
-
-variable "aws_es_username" {
-  type = string
-}
-
-variable "aws_es_password" {
-  type = string
-}
-
-variable "geojson_s3_bucket" {
-  default = "syrf-rawdataserver-geojson-staging"
-  type    = string
-}
-
-variable "yellowbrick_kml_s3_bucket" {
-  default = "syrf-rawdataserver-yellowbrick-kml-staging"
-  type    = string
-}
