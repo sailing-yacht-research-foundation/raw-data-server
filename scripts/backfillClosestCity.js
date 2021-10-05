dda07e6b2fd459a29179b63d6b959f2a9915b7da
AlvinJK <alvin.junta@vospay.id>
2021-10-05 14:38:21 +0700

    Remove loopbreak, add transaction

--- scripts/backfillClosestCity.js
+++ scripts/backfillClosestCity.js
@@ -1,17 +1,19 @@
 require('dotenv').config();
 const db = require('../src/models');
 const elasticsearch = require('../src/utils/elasticsearch');
+const Op = db.Sequelize.Op;
 
 const { pointToCity } = require('../src/utils/gisUtils');
 
 (async () => {
   let page = 1;
-  const perPage = 5;
+  const perPage = 10;
 
   let shouldContinue = true;
+  const failedIds = [];
   do {
     const data = await db.readyAboutRaceMetadata.findAll({
-      where: { start_city: null },
+      where: { start_city: null, id: { [Op.notIn]: failedIds } },
       raw: true,
       offset: (page - 1) * perPage,
       limit: perPage,
@@ -20,19 +22,22 @@ const { pointToCity } = require('../src/utils/gisUtils');
 
     if (data.length > 0) {
       for (let i = 0; i < data.length; i++) {
+        const transaction = await db.sequelize.transaction();
+        const { id, approx_start_point: startPoint } = data[i];
         try {
-          const { id, approx_start_point: startPoint } = data[i];
           const closestCity = pointToCity(startPoint.coordinates);
           await db.readyAboutRaceMetadata.update(
             { start_city: closestCity },
-            { where: { id } },
+            { where: { id }, transaction },
           );
           await elasticsearch.updateRace(id, {
             start_city: closestCity,
           });
+          await transaction.commit();
         } catch (error) {
           console.error('Failed updating:', error.message);
-          shouldContinue = false;
+          failedIds.push(id);
+          await transaction.rollback();
         }
       }
       console.log('Done updating:', data.map((row) => row.id).join(', '));
