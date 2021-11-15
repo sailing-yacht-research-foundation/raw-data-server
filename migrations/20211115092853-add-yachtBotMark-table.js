6a815f5af67af36f8cb1d758345b5b7b2c294c88
Tung Nguyen <nguyenmanhtung848@gmail.com>
2021-11-15 17:12:23 +0700

    Nullcheck for migration YachtBotMarks

--- migrations/20211115092853-add-yachtBotMark-table.js
+++ migrations/20211115092853-add-yachtBotMark-table.js
@@ -2,54 +2,62 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    let tableInfo;
+    try {
+      tableInfo = await queryInterface.describeTable('YachtBotMarks');
+    } catch (err) {
+      tableInfo = null;
+    }
 
-    await queryInterface.createTable('YachtBotMarks', {
-      id: {
-        type: Sequelize.UUID,
-        allowNull: false,
-        primaryKey: true,
-      },
-      original_id: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-      },
-      race: {
-        type: Sequelize.UUID,
-        allowNull: false,
-      },
-      race_original_id: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-      },
-      colour: {
-        type: Sequelize.TEXT,
-        allowNull: true,
-      },
-      name: {
-        type: Sequelize.TEXT,
-        allowNull: true,
-      },
-      type: {
-        type: Sequelize.TEXT,
-        allowNull: true,
-      },
-      connected_buoy: {
-        type: Sequelize.TEXT,
-        allowNull: true,
-      },
-      connected_buoy_original_id: {
-        type: Sequelize.TEXT,
-        allowNull: true,
-      },
-      lat: {
-        type: Sequelize.TEXT,
-        allowNull: true,
-      },
-      lon: {
-        type: Sequelize.TEXT,
-        allowNull: true,
-      },
-    });
+    if (!tableInfo) {
+      await queryInterface.createTable('YachtBotMarks', {
+        id: {
+          type: Sequelize.UUID,
+          allowNull: false,
+          primaryKey: true,
+        },
+        original_id: {
+          type: Sequelize.TEXT,
+          allowNull: false,
+        },
+        race: {
+          type: Sequelize.UUID,
+          allowNull: false,
+        },
+        race_original_id: {
+          type: Sequelize.TEXT,
+          allowNull: false,
+        },
+        colour: {
+          type: Sequelize.TEXT,
+          allowNull: true,
+        },
+        name: {
+          type: Sequelize.TEXT,
+          allowNull: true,
+        },
+        type: {
+          type: Sequelize.TEXT,
+          allowNull: true,
+        },
+        connected_buoy: {
+          type: Sequelize.TEXT,
+          allowNull: true,
+        },
+        connected_buoy_original_id: {
+          type: Sequelize.TEXT,
+          allowNull: true,
+        },
+        lat: {
+          type: Sequelize.TEXT,
+          allowNull: true,
+        },
+        lon: {
+          type: Sequelize.TEXT,
+          allowNull: true,
+        },
+      });
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
