00bd064c4aee2020c4ddee6487450fb2e04c2bc7
Tung Nguyen <nguyenmanhtung848@gmail.com>
2021-11-26 17:56:43 +0700

    SNS-481: add transaction in migration script for geovoile tables

--- migrations/20211126071903-update-geovoile-tables.js
+++ migrations/20211126071903-update-geovoile-tables.js
@@ -8,72 +8,87 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-    let tableInfo;
-    try {
-      tableInfo = await queryInterface.describeTable('GeovoileMarks');
-    } catch (err) {
-      tableInfo = null;
-    }
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      let tableInfo;
+      try {
+        tableInfo = await queryInterface.describeTable('GeovoileMarks');
+      } catch (err) {
+        tableInfo = null;
+      }
 
-    if (!tableInfo) {
-      await queryInterface.createTable('GeovoileMarks', {
-        id: {
-          type: Sequelize.UUID,
-          defaultValue: Sequelize.UUIDV1,
-          allowNull: false,
-          primaryKey: true,
-        },
-        race_id: {
-          type: Sequelize.UUID,
-          allowNull: false,
-        },
-        race_original_id: {
-          type: Sequelize.UUID,
-          allowNull: true,
-        },
-        name: {
-          type: Sequelize.STRING,
-          allowNull: true,
-        },
-        type: {
-          type: Sequelize.STRING,
-          allowNull: true,
-        },
-        lon: {
-          type: Sequelize.DOUBLE,
-          allowNull: true,
-        },
-        lat: {
-          type: Sequelize.DOUBLE,
-          allowNull: true,
-        },
-        xy: {
-          type: Sequelize.ARRAY(Sequelize.TEXT),
-          allowNull: true,
-        },
-      });
-    }
+      if (!tableInfo) {
+        await queryInterface.createTable(
+          'GeovoileMarks',
+          {
+            id: {
+              type: Sequelize.UUID,
+              defaultValue: Sequelize.UUIDV1,
+              allowNull: false,
+              primaryKey: true,
+            },
+            race_id: {
+              type: Sequelize.UUID,
+              allowNull: false,
+            },
+            race_original_id: {
+              type: Sequelize.UUID,
+              allowNull: true,
+            },
+            name: {
+              type: Sequelize.STRING,
+              allowNull: true,
+            },
+            type: {
+              type: Sequelize.STRING,
+              allowNull: true,
+            },
+            lon: {
+              type: Sequelize.DOUBLE,
+              allowNull: true,
+            },
+            lat: {
+              type: Sequelize.DOUBLE,
+              allowNull: true,
+            },
+            xy: {
+              type: Sequelize.ARRAY(Sequelize.TEXT),
+              allowNull: true,
+            },
+          },
+          { transaction },
+        );
+      }
 
-    const tableGeovoileBoats = await queryInterface.describeTable(
-      'GeovoileBoats',
-    );
+      const tableGeovoileBoats = await queryInterface.describeTable(
+        'GeovoileBoats',
+      );
 
-    if (!tableGeovoileBoats.arrival) {
-      await queryInterface.addColumn('GeovoileBoats', 'arrival', {
-        type: Sequelize.JSON,
-        allowNull: true,
-      });
-    }
+      if (!tableGeovoileBoats.arrival) {
+        await queryInterface.addColumn(
+          'GeovoileBoats',
+          'arrival',
+          {
+            type: Sequelize.JSON,
+            allowNull: true,
+          },
+          { transaction },
+        );
+      }
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-    await queryInterface.dropTable('GeovoileMarks');
-    await queryInterface.removeColumn('GeovoileBoats', 'arrival');
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      /**
+       * Add reverting commands here.
+       *
+       * Example:
+       * await queryInterface.dropTable('users');
+       */
+      await queryInterface.dropTable('GeovoileMarks', { transaction });
+      await queryInterface.removeColumn('GeovoileBoats', 'arrival', {
+        transaction,
+      });
+    });
   },
 };
