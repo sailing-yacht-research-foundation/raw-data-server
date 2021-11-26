68ea753d20e19d64a7443fcba3728d0c3193fdb1
Olufemi Adewale <35527059+platof@users.noreply.github.com>
2021-11-26 16:12:33 +0300

    Cicd prod (#102)
    * change in src/main.js
    
    * prod cicd for rawdata server
    
    Co-authored-by: femi <femmie009@gmail.com>

---
+++ deployment/prod/alb.tf
@@ -0,0 +1,62 @@
+resource "aws_alb" "application_load_balancer" {
+  name               = "Raw-Data-Server-LB"
+  load_balancer_type = "application"
+  subnets            = aws_subnet.public_subnet.*.id
+  security_groups    = [aws_security_group.load_balancer_security_group.id]
+}
+
+resource "aws_security_group" "load_balancer_security_group" {
+  vpc_id = aws_vpc.syrf-vpc.id
+  ingress {
+    from_port   = 80 # Allowing traffic in from port 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
+  }
+
+  ingress {
+    from_port   = 443 # For https
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 2049 # For EFS
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0             # Allowing any incoming port
+    to_port     = 0             # Allowing any outgoing port
+    protocol    = "-1"          # Allowing any outgoing protocol 
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "raw-server-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.syrf-vpc.id
+  health_check {
+    matcher  = "200,301,302"
+    path     = "/"
+    timeout  = 30
+    interval = 60
+  }
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_alb.application_load_balancer.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
