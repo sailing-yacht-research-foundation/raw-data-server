2a202a16141f2838ed64b70cd439bb86400d466c
Jackson Tan <jackson.tan.s@gmail.com>
2021-12-22 22:23:50 +0800

    SNS-637 - Bluewater backfill script to save in syrf mainDB

--- src/services/saveBluewaterData.js
+++ src/services/saveBluewaterData.js
@@ -12,93 +12,130 @@ const saveBluewaterData = async (data) => {
   let errorMessage = '';
   let raceUrl = [];
   let raceMetadata;
-  try {
-    if (data.BluewaterRace) {
-      raceUrl = data.BluewaterRace.map((row) => {
-        // TODO: Bluewater currently don't have url on race, adding slug temporarily
-        return { url: row.slug, original_id: row.original_id };
-      });
-      await db.bluewaterRace.bulkCreate(data.BluewaterRace, {
-        ignoreDuplicates: true,
-        validate: true,
-        transaction,
-      });
-    }
-    if (data.BluewaterBoat) {
-      await db.bluewaterBoat.bulkCreate(data.BluewaterBoat, {
-        ignoreDuplicates: true,
-        validate: true,
-        transaction,
-      });
-    }
-    if (data.BluewaterBoatHandicap) {
-      await db.bluewaterBoatHandicap.bulkCreate(data.BluewaterBoatHandicap, {
-        ignoreDuplicates: true,
-        validate: true,
-        transaction,
-      });
-    }
-    if (data.BluewaterBoatSocialMedia) {
-      await db.bluewaterBoatSocialMedia.bulkCreate(
-        data.BluewaterBoatSocialMedia,
-        {
-          ignoreDuplicates: true,
-          validate: true,
-          transaction,
-        },
-      );
-    }
-    if (data.BluewaterCrew) {
-      await db.bluewaterCrew.bulkCreate(data.BluewaterCrew, {
-        ignoreDuplicates: true,
-        validate: true,
-        transaction,
-      });
-    }
-    if (data.BluewaterCrewSocialMedia) {
-      await db.bluewaterCrewSocialMedia.bulkCreate(
-        data.BluewaterCrewSocialMedia,
-        {
-          ignoreDuplicates: true,
-          validate: true,
-          transaction,
-        },
-      );
-    }
-    if (data.BluewaterMap) {
-      await db.bluewaterMap.bulkCreate(data.BluewaterMap, {
-        ignoreDuplicates: true,
-        validate: true,
-        transaction,
-      });
-    }
-    if (data.BluewaterPosition) {
-      const positions = data.BluewaterPosition.slice(); // clone array to avoid mutating the data
-      while (positions.length > 0) {
-        const splicedArray = positions.splice(0, SAVE_DB_POSITION_CHUNK_COUNT);
-        await db.bluewaterPosition.bulkCreate(splicedArray, {
+  if (process.env.ENABLE_MAIN_DB_SAVE_BLUEWATER !== 'true') {
+    try {
+      if (data.BluewaterRace) {
+        raceUrl = data.BluewaterRace.map((row) => {
+          // TODO: Bluewater currently don't have url on race, adding slug temporarily
+          return { url: row.slug, original_id: row.original_id };
+        });
+        await db.bluewaterRace.bulkCreate(data.BluewaterRace, {
           ignoreDuplicates: true,
           validate: true,
           transaction,
         });
       }
-    }
-    if (data.BluewaterAnnouncement) {
-      await db.bluewaterAnnouncement.bulkCreate(data.BluewaterAnnouncement, {
-        ignoreDuplicates: true,
-        validate: true,
-        transaction,
-      });
+      if (data.BluewaterBoat) {
+        await db.bluewaterBoat.bulkCreate(data.BluewaterBoat, {
+          ignoreDuplicates: true,
+          validate: true,
+          transaction,
+        });
+      }
+      if (data.BluewaterBoatHandicap) {
+        await db.bluewaterBoatHandicap.bulkCreate(data.BluewaterBoatHandicap, {
+          ignoreDuplicates: true,
+          validate: true,
+          transaction,
+        });
+      }
+      if (data.BluewaterBoatSocialMedia) {
+        await db.bluewaterBoatSocialMedia.bulkCreate(
+          data.BluewaterBoatSocialMedia,
+          {
+            ignoreDuplicates: true,
+            validate: true,
+            transaction,
+          },
+        );
+      }
+      if (data.BluewaterCrew) {
+        await db.bluewaterCrew.bulkCreate(data.BluewaterCrew, {
+          ignoreDuplicates: true,
+          validate: true,
+          transaction,
+        });
+      }
+      if (data.BluewaterCrewSocialMedia) {
+        await db.bluewaterCrewSocialMedia.bulkCreate(
+          data.BluewaterCrewSocialMedia,
+          {
+            ignoreDuplicates: true,
+            validate: true,
+            transaction,
+          },
+        );
+      }
+      if (data.BluewaterMap) {
+        await db.bluewaterMap.bulkCreate(data.BluewaterMap, {
+          ignoreDuplicates: true,
+          validate: true,
+          transaction,
+        });
+      }
+      if (data.BluewaterPosition) {
+        const positions = data.BluewaterPosition.slice(); // clone array to avoid mutating the data
+        while (positions.length > 0) {
+          const splicedArray = positions.splice(
+            0,
+            SAVE_DB_POSITION_CHUNK_COUNT,
+          );
+          await db.bluewaterPosition.bulkCreate(splicedArray, {
+            ignoreDuplicates: true,
+            validate: true,
+            transaction,
+          });
+        }
+      }
+      if (data.BluewaterAnnouncement) {
+        await db.bluewaterAnnouncement.bulkCreate(data.BluewaterAnnouncement, {
+          ignoreDuplicates: true,
+          validate: true,
+          transaction,
+        });
+      }
+
+      if (data.BluewaterRace) {
+        raceMetadata = await normalizeRace(data, transaction);
+      }
+      await transaction.commit();
+    } catch (error) {
+      console.log(error);
+      await transaction.rollback();
+      errorMessage = databaseErrorHandler(error);
     }
 
-    if (data.BluewaterRace) {
-      raceMetadata = await normalizeRace(data, transaction);
+    if (raceUrl.length > 0) {
+      if (errorMessage) {
+        await db.bluewaterFailedUrl.bulkCreate(
+          raceUrl.map((row) => {
+            return {
+              id: uuidv4(),
+              url: row.url,
+              error: errorMessage,
+            };
+          }),
+          {
+            ignoreDuplicates: true,
+            validate: true,
+          },
+        );
+      } else {
+        await db.bluewaterSuccessfulUrl.bulkCreate(
+          raceUrl.map((row) => {
+            return {
+              id: uuidv4(),
+              url: row.url,
+              original_id: row.original_id,
+            };
+          }),
+          {
+            ignoreDuplicates: true,
+            validate: true,
+          },
+        );
+      }
     }
-    await transaction.commit();
-  } catch (error) {
-    console.log(error);
-    await transaction.rollback();
-    errorMessage = databaseErrorHandler(error);
   }
 
   // temporary add of test env to avoid accidentally saving on maindb until its mocked
@@ -110,38 +147,6 @@ const saveBluewaterData = async (data) => {
       await mapAndSave(data, raceMetadata);
     } catch (err) {
       console.log(err);
-    }
-  }
-
-  if (raceUrl.length > 0) {
-    if (errorMessage) {
-      await db.bluewaterFailedUrl.bulkCreate(
-        raceUrl.map((row) => {
-          return {
-            id: uuidv4(),
-            url: row.url,
-            error: errorMessage,
-          };
-        }),
-        {
-          ignoreDuplicates: true,
-          validate: true,
-        },
-      );
-    } else {
-      await db.bluewaterSuccessfulUrl.bulkCreate(
-        raceUrl.map((row) => {
-          return {
-            id: uuidv4(),
-            url: row.url,
-            original_id: row.original_id,
-          };
-        }),
-        {
-          ignoreDuplicates: true,
-          validate: true,
-        },
-      );
     }
   }
 
