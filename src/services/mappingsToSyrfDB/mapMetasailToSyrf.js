250687c78cfc271ce3b507334c9d8085c72366b8
Tung Nguyen <nguyenmanhtung848@gmail.com>
2021-12-29 14:20:16 +0700

    SNS-485: remove name from vesselId, add | as separator

--- src/services/mappingsToSyrfDB/mapMetasailToSyrf.js
+++ src/services/mappingsToSyrfDB/mapMetasailToSyrf.js
@@ -86,7 +86,7 @@ const _mapBoats = (boats = [], race) => {
         id: b.id,
         publicName: b.name,
         globalId: b.sail_number,
-        vesselId: `${b.original_id}-${b.serial}-${b.sail_number}-${b.name}`,
+        vesselId: `${b.original_id}|${b.serial}|${b.sail_number}`,
         model: b.class_name,
       };
       return vessel;
