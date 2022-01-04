d7b8552f41b3b5da16fd0dc76598f3bb98de37b4
Nguyen Manh Tung <nguyenmanhtung848@gmail.com>
2022-01-04 16:37:18 +0700

    Feature/sns 486 mapping raceqs data (#112)
    * SNS-486: mapping RaceQs to main database
    
    * SNS-486: fix position timestamp, rankings, and course sequence
    
    * SNS-486: Add try catch, RaceQsEvent to race, RaceQsRegatta to event
    
    * SNS-486: fix mapping positions, _mapSequencedGeometries
    
    * SNS-486: use raceQsRace  instead of accessing data.RaceQsEvent[0] every time
    
    * SNS-486: change raceQsRace variable name to race
    
    * SNS-486: mapping raceqs data with RaceQsDivisions, RaceQsStarts, RaceQsRoutes
    
    * SNS-486: reduce duplicate positions, for each start, create separate competition unit
    
    * SNS-486: format race name with time zone, use start to group input boat
    
    * SNS-486: fix map rankings, map order
    
    * SNS-486: new way to normalize race qs, save competition unit with handicap data.
    
    * SNS-486: update get racename in normalize raceqs, update find positions condition in raceqs
    
    * SNS-486: flow for backfilling raceqs to main db
    
    * SNS-486: fix small bugs for normalize RaceQs, mapRaceQs to main, add saveQsTomainDB script
    
    * SNS-486: fix query problem for raceQsSaveToMainDB
    
    * SNS-486: fix normalize race qs start time
    
    * SNS-486: add condition to filter division without starts
    
    * SNS-486: update mapping logic save original id to indicate which original is taken (start or divsion), update race qs save to main db to filter out the saved original ids
    
    * SNS-486: seprate old code block from save to raw db and save to main db
    
    * SNS-486: put the raceqs-start in a constant var
    
    * SNS-486: change variable name raceQsRegatta, remove the transaction in the normalizeRace
    
    * SNS-486: Should not upload the geojson and save to tracker db if the syrf main db is enabled.

---
+++ scripts/saveToMainDB/raceQsSaveToMainDB.js
@@ -0,0 +1,139 @@
+require('dotenv').config();
+const db = require('../../src/models');
+const Op = db.Sequelize.Op;
+const { SOURCE, RACEQS } = require('../../src/constants');
+const { getExistingData } = require('../../src/services/scrapedDataResult');
+const mapRaceQsToSyrf = require('../../src/services/mappingsToSyrfDB/mapRaceQsToSyrf');
+const {
+  normalizeRace,
+} = require('../../src/services/normalization/normalizeRaceQs');
+const elasticsearch = require('../../src/utils/elasticsearch');
+(async () => {
+  const limit = 10;
+  let page = 0;
+  let shouldContinue = true;
+  const existingData = await getExistingData(SOURCE.RACEQS);
+
+  const savedStartOriginalIds = new Set(
+    existingData
+      .map((t) => t.original_id)
+      .filter((t) => {
+        return t.indexOf(RACEQS.START_PREFIX) !== -1;
+      })
+      .map((t) => t.replace(RACEQS.START_PREFIX, '')),
+  );
+  const savedDivisionOriginalIds = existingData
+    .map((t) => t.original_id)
+    .filter((t) => {
+      return t.indexOf(RACEQS.DIVISION_PREFIX) !== -1;
+    })
+    .map((t) => t.replace(RACEQS.DIVISION_PREFIX, ''));
+  while (shouldContinue) {
+    const raceQsEvents = await db.raceQsEvent.findAll({
+      raw: true,
+      limit,
+      offset: page * limit,
+      order: [['original_id', 'ASC']],
+    });
+    if (raceQsEvents.length === 0) {
+      shouldContinue = false;
+      break;
+    }
+
+    console.log(`Processing ${raceQsEvents.length} raceQsEvents`);
+    for (const raceQsEvent of raceQsEvents) {
+      try {
+        console.log(
+          `Processing raceQsEvents with original_id = ${raceQsEvent.original_id}`,
+        );
+        const raceFilter = {
+          where: {
+            event: raceQsEvent.id,
+            original_id: {
+              [Op.notIn]: savedDivisionOriginalIds,
+            },
+          },
+          raw: true,
+        };
+        const raceQsDivision = await db.raceQsDivision.findAll(raceFilter);
+
+        if (!raceQsDivision.length) {
+          console.log(
+            `this raceQsEvent = ${raceQsEvent.original_id} is already processed, continue to next event`,
+          );
+          continue;
+        }
+        const eventFilter = {
+          where: {
+            event: raceQsEvent.id,
+          },
+          raw: true,
+        };
+        // we only filter by event to make sure that the division does not have start check
+        let raceQsStart = await db.raceQsStart.findAll(eventFilter);
+
+        // filter out processed start
+        if (raceQsStart.length > 0) {
+          raceQsStart = raceQsStart.filter(
+            (t) => !savedStartOriginalIds.has(t.original_id),
+          );
+
+          if (!raceQsStart.length) {
+            continue;
+          }
+        }
+        const raceQsRegatta = await db.raceQsRegatta.findAll({
+          where: {
+            id: raceQsEvent.regatta,
+          },
+          raw: true,
+        });
+
+        const raceQsPosition = await db.raceQsPosition.findAll(eventFilter);
+
+        const raceQsParticipant = await db.raceQsParticipant.findAll(
+          eventFilter,
+        );
+        const raceQsRoute = await db.raceQsRoute.findAll(eventFilter);
+
+        const raceQsWaypoint = await db.raceQsWaypoint.findAll(eventFilter);
+
+        const objectToPass = {
+          RaceQsEvent: [raceQsEvent],
+          RaceQsRegatta: raceQsRegatta,
+          RaceQsPosition: raceQsPosition,
+          RaceQsDivision: raceQsDivision,
+          RaceQsParticipant: raceQsParticipant,
+          RaceQsRoute: raceQsRoute,
+          RaceQsStart: raceQsStart,
+          RaceQsWaypoint: raceQsWaypoint,
+        };
+        try {
+          // in the old code of saveRaceQsData.js
+          // we normalize the race by raceQsEvent, but it is not true
+          // in the logic,  we normalize the race by division and start
+          // in case there is a start in division, raceId = start.id
+          // in case there is no start, raceId = division.id
+          const raceMetadatas = await normalizeRace(objectToPass);
+          await mapRaceQsToSyrf(objectToPass, raceMetadatas);
+          console.log('Finished saving race');
+          // Since we change the logic of normalizeRace, so we need to delete the raceQsEvent.id
+          try {
+            await elasticsearch.deleteByIds([raceQsEvent.id]);
+          } catch (e) {
+            console.log(
+              `Failed to delete raceQsEvent.id = ${raceQsEvent.id} from elastic search`,
+            );
+            console.log(e);
+          }
+        } catch (err) {
+          console.log('Failed saving to syrf main DB', err);
+        }
+      } catch (err) {
+        console.log('Error occured getting regatta data', err);
+      }
+    }
+    page++;
+  }
+  console.log('Finished saving all scraper db data to main db');
+})();
--- src/services/normalization/normalizeRaceQs.js
+++ src/services/normalization/normalizeRaceQs.js
@@ -12,6 +12,8 @@ const {
   allPositionsToFeatureCollection,
 } = require('../../utils/gisUtils');
 const { uploadGeoJsonToS3 } = require('../uploadUtil');
+const THRESHOLD_TIME = 600000;
+const moment = require('moment');
 
 const normalizeRace = async (
   {
@@ -20,6 +22,9 @@ const normalizeRace = async (
     RaceQsWaypoint,
     RaceQsPosition,
     RaceQsParticipant,
+    RaceQsDivision,
+    RaceQsStart,
+    RaceQsRoute,
   },
   transaction,
 ) => {
@@ -31,107 +36,177 @@ const normalizeRace = async (
     console.log('No event or positions so skipping.');
     return;
   }
+  const availablePositions = [];
+  RaceQsPosition.forEach((p) => {
+    if (p.lat && p.lon && p.time) {
+      p.timestamp = parseInt(p.time) * 100;
+      availablePositions.push(p);
+    }
+  });
+  availablePositions.sort((a, b) => a.timestamp - b.timestamp);
 
-  for (const event of RaceQsEvent) {
-    const allPositions = [];
-    RaceQsPosition.forEach((p) => {
-      if (
-        p.event_original_id === event.original_id &&
-        p.lat &&
-        p.lon &&
-        p.time
-      ) {
-        p.timestamp = parseInt(p.time) * 100;
-        allPositions.push(p);
-      }
-    });
+  if (availablePositions.length === 0) {
+    console.log('No event positions so skipping.');
+    return;
+  }
+  const event = RaceQsEvent[0];
+  for (const [index, division] of RaceQsDivision.entries()) {
+    const starts = RaceQsStart?.filter((t) => t.division === division.id) || [];
 
-    if (allPositions.length === 0) {
-      console.log('No event positions so skipping.');
+    // if there is no starts for this division, and the event does have many starts
+    // ignore current division
+    if (!starts?.length && RaceQsStart?.length) {
       continue;
     }
-    const waypoints = RaceQsWaypoint?.filter((w) => w.event_original_id === event.original_id);
-    const participants = RaceQsParticipant?.filter((p) => p.event_original_id === event.original_id);
-    const id = event.id;
-    const url = event.url;
-    const startTime = parseInt(event.from);
-    const endTime = parseInt(event.till);
+    // if there is no start so we will take the first division only
+    if ((!RaceQsStart || !RaceQsStart?.length) && index > 0) {
+      continue;
+    }
+    do {
+      const start = starts.shift();
+      const waypoints = _getWayPoints(start, RaceQsWaypoint, RaceQsRoute);
 
-    const boatIdentifiers = [];
-    const boatNames = [];
-    const unstructuredText = [];
-    const classes = [];
-    const handicaps = [];
-    participants.forEach((p) => {
-      boatNames.push(p.boat);
-    });
+      const allPositions = _getAllPositions(availablePositions, start);
 
-    let startPoint = null;
-    let endPoint = null;
-    waypoints?.forEach((wpt) => {
-      if (wpt.type === 'Start') {
-        startPoint = createTurfPoint(wpt.lat, wpt.lon);
-      } else if (wpt.type === 'Finish') {
-        endPoint = createTurfPoint(wpt.lat, wpt.lon);
+      if (!allPositions.length) {
+        console.log(
+          `No positions for start ${start?.id}, from = ${start?.from} is found`,
+        );
+        continue;
       }
-    });
-
-    const fc = positionsToFeatureCollection('lat', 'lon', allPositions);
-    const boundingBox = turf.bbox(fc);
-    const boatsToSortedPositions = createBoatToPositionDictionary(
-      allPositions,
-      'participant',
-      'timestamp',
-    );
-
-    if (!startPoint) {
-      const first3Positions = collectFirstNPositionsFromBoatsToPositions(
-        boatsToSortedPositions,
-        3,
+      let startPoint = null;
+      let endPoint = null;
+      waypoints?.forEach((wpt) => {
+        if (wpt.type === 'Start') {
+          startPoint = createTurfPoint(wpt.lat, wpt.lon);
+        } else if (wpt.type === 'Finish') {
+          endPoint = createTurfPoint(wpt.lat, wpt.lon);
+        }
+      });
+      const participants = _getParticipants(start, RaceQsParticipant);
+      const id = start?.id || division.id;
+      const url = event.url;
+      const startTime = start ? parseInt(start.from) : parseInt(event.from);
+      const endTime = parseInt(event.till);
+      const boatIdentifiers = [];
+      const boatNames = [];
+      const unstructuredText = [];
+      const classes = [];
+      const handicaps = [];
+      if (start?.type) {
+        handicaps.push(start.type);
+      }
+      participants.forEach((p) => {
+        boatNames.push(p.boat);
+      });
+      const fc = positionsToFeatureCollection('lat', 'lon', allPositions);
+      const boundingBox = turf.bbox(fc);
+      const boatsToSortedPositions = createBoatToPositionDictionary(
+        allPositions,
+        'participant',
+        'timestamp',
       );
-      startPoint = getCenterOfMassOfPositions('lat', 'lon', first3Positions);
-    }
-    if (!endPoint) {
-      const last3Positions = collectLastNPositionsFromBoatsToPositions(
+      if (!startPoint) {
+        const first3Positions = collectFirstNPositionsFromBoatsToPositions(
+          boatsToSortedPositions,
+          3,
+        );
+        startPoint = getCenterOfMassOfPositions('lat', 'lon', first3Positions);
+      }
+      if (!endPoint) {
+        const last3Positions = collectLastNPositionsFromBoatsToPositions(
+          boatsToSortedPositions,
+          3,
+        );
+        endPoint = getCenterOfMassOfPositions('lat', 'lon', last3Positions);
+      }
+      const roughLength = findAverageLength(
+        'lat',
+        'lon',
         boatsToSortedPositions,
-        3,
       );
-      endPoint = getCenterOfMassOfPositions('lat', 'lon', last3Positions);
-    }
-
-    const roughLength = findAverageLength('lat', 'lon', boatsToSortedPositions);
-    const raceMetadata = await createRace(
-      id,
-      event.name, // for raceQs event is race
-      regatta?.name,
-      regatta?.id,
-      RACEQS_SOURCE,
-      url,
-      startTime,
-      endTime,
-      startPoint,
-      endPoint,
-      boundingBox,
-      roughLength,
-      boatsToSortedPositions,
-      boatNames,
-      classes,
-      boatIdentifiers,
-      handicaps,
-      unstructuredText,
-    );
-
-    const tracksGeojson = JSON.stringify(
-      allPositionsToFeatureCollection(boatsToSortedPositions),
-    );
-    await db.readyAboutRaceMetadata.create(raceMetadata, {
-      fields: Object.keys(raceMetadata),
-      transaction,
-    });
-    await uploadGeoJsonToS3(id, tracksGeojson, RACEQS_SOURCE, transaction);
-    raceMetadatas.push(raceMetadata);
+      const raceName = _getRaceName(regatta, division, start, event);
+      const raceMetadata = await createRace(
+        id,
+        raceName, // for raceQs event is race
+        regatta?.name,
+        regatta?.id,
+        RACEQS_SOURCE,
+        url,
+        startTime,
+        endTime,
+        startPoint,
+        endPoint,
+        boundingBox,
+        roughLength,
+        boatsToSortedPositions,
+        boatNames,
+        classes,
+        boatIdentifiers,
+        handicaps,
+        unstructuredText,
+      );
+      if (process.env.ENABLE_MAIN_DB_SAVE_RACEQS !== 'true') {
+        const tracksGeojson = JSON.stringify(
+          allPositionsToFeatureCollection(boatsToSortedPositions),
+        );
+        await db.readyAboutRaceMetadata.create(raceMetadata, {
+          fields: Object.keys(raceMetadata),
+          transaction,
+        });
+        await uploadGeoJsonToS3(id, tracksGeojson, RACEQS_SOURCE, transaction);
+      }
+      raceMetadatas.push(raceMetadata);
+    } while (starts.length);
   }
   return raceMetadatas;
 };
 
+const _getWayPoints = (start, raceQsWaypoint, raceQsRoute) => {
+  if (!start) {
+    return [];
+  }
+  const availableWayPoints = new Set(
+    raceQsRoute.filter((t) => t.start === start.id).map((t) => t.waypoint),
+  );
+  return raceQsWaypoint.filter((t) => availableWayPoints.has(t.id));
+};
+
+const _getParticipants = (start, raceQsParticipant) => {
+  return raceQsParticipant
+    ?.filter((b) => {
+      if (start && b.start && b.finish) {
+        const boatStartTime = new Date(b.start);
+        const boatFinishTime = new Date(b.finish);
+        if (
+          boatStartTime.getTime() <= start.from &&
+          boatFinishTime >= start.from
+        ) {
+          return b;
+        }
+        return null;
+      }
+      return b;
+    })
+    .filter((t) => t);
+};
+
+const _getAllPositions = (allPositions, start) => {
+  if (!start) {
+    return allPositions;
+  }
+  const lowestTime = start.from - THRESHOLD_TIME;
+
+  return allPositions.filter((t) => t.timestamp >= lowestTime);
+};
+const _getRaceName = (regatta, division, start, raceQsEvent) => {
+  const raceNames = [regatta.name, division.name];
+
+  if (start?.from && raceQsEvent.tz) {
+    const startTime = moment(start.from).utcOffset(raceQsEvent.tz);
+    raceNames.push(startTime.format('HH:mm:ss'));
+  }
+
+  return raceNames.filter((t) => t).join(' - ');
+};
 exports.normalizeRace = normalizeRace;
