86346b2e387f348c736f7ea1ccce66dc892bc2db
Jackson Tan <jackson.tan.s@gmail.com>
2022-01-20 17:00:05 +0800

    SNS-649 - Add yellowbrick script to convert existing data to syrf main DB Modify competition unit handicap column to an array so store multiple handicaps Add bluewater handicap storing on competitionUnit

--- src/services/saveYellowbrickData.js
+++ src/services/saveYellowbrickData.js
@@ -13,83 +13,120 @@ const saveYellowbrickData = async (data) => {
   let errorMessage = '';
   let raceUrl = [];
   let raceMetadata;
-  try {
-    if (data.YellowbrickRace) {
-      raceUrl = data.YellowbrickRace.map((row) => {
-        return { url: row.url, original_id: row.race_code };
-      });
-      await db.yellowbrickRace.bulkCreate(data.YellowbrickRace, {
-        ignoreDuplicates: true,
-        validate: true,
-        transaction,
-      });
-    }
-    if (data.YellowbrickCourseNode) {
-      await db.yellowbrickCourseNode.bulkCreate(data.YellowbrickCourseNode, {
-        ignoreDuplicates: true,
-        validate: true,
-        transaction,
-      });
-    }
-    if (data.YellowbrickLeaderboardTeam) {
-      await db.yellowbrickLeaderboardTeam.bulkCreate(
-        data.YellowbrickLeaderboardTeam,
-        {
-          ignoreDuplicates: true,
-          validate: true,
-          transaction,
-        },
-      );
-    }
-    if (data.YellowbrickPoi) {
-      await db.yellowbrickPoi.bulkCreate(data.YellowbrickPoi, {
-        ignoreDuplicates: true,
-        validate: true,
-        transaction,
-      });
-    }
-    if (data.YellowbrickPosition) {
-      const positions = data.YellowbrickPosition.slice(); // clone array to avoid mutating the data
-      while (positions.length > 0) {
-        const splicedArray = positions.splice(0, SAVE_DB_POSITION_CHUNK_COUNT);
-        await db.yellowbrickPosition.bulkCreate(splicedArray, {
+  if (process.env.ENABLE_MAIN_DB_SAVE_YELLOWBRICK !== 'true') {
+    try {
+      if (data.YellowbrickRace) {
+        raceUrl = data.YellowbrickRace.map((row) => {
+          return { url: row.url, original_id: row.race_code };
+        });
+        await db.yellowbrickRace.bulkCreate(data.YellowbrickRace, {
           ignoreDuplicates: true,
           validate: true,
           transaction,
         });
       }
-    }
-    if (data.YellowbrickTag) {
-      await db.yellowbrickTag.bulkCreate(data.YellowbrickTag, {
-        ignoreDuplicates: true,
-        validate: true,
-        transaction,
-      });
-    }
-    if (data.YellowbrickTeam) {
-      await db.yellowbrickTeam.bulkCreate(data.YellowbrickTeam, {
-        ignoreDuplicates: true,
-        validate: true,
-        transaction,
-      });
-    }
-    if (data.YellowbrickKml) {
-      for (const kmlObj of data.YellowbrickKml) {
-        await uploadUtil.uploadDataToS3({
-          Bucket: KML_S3_BUCKET,
-          Key: `${kmlObj.id}.kml`,
-          Body: kmlObj.data,
+      if (data.YellowbrickCourseNode) {
+        await db.yellowbrickCourseNode.bulkCreate(data.YellowbrickCourseNode, {
+          ignoreDuplicates: true,
+          validate: true,
+          transaction,
         });
       }
+      if (data.YellowbrickLeaderboardTeam) {
+        await db.yellowbrickLeaderboardTeam.bulkCreate(
+          data.YellowbrickLeaderboardTeam,
+          {
+            ignoreDuplicates: true,
+            validate: true,
+            transaction,
+          },
+        );
+      }
+      if (data.YellowbrickPoi) {
+        await db.yellowbrickPoi.bulkCreate(data.YellowbrickPoi, {
+          ignoreDuplicates: true,
+          validate: true,
+          transaction,
+        });
+      }
+      if (data.YellowbrickPosition) {
+        const positions = data.YellowbrickPosition.slice(); // clone array to avoid mutating the data
+        while (positions.length > 0) {
+          const splicedArray = positions.splice(
+            0,
+            SAVE_DB_POSITION_CHUNK_COUNT,
+          );
+          await db.yellowbrickPosition.bulkCreate(splicedArray, {
+            ignoreDuplicates: true,
+            validate: true,
+            transaction,
+          });
+        }
+      }
+      if (data.YellowbrickTag) {
+        await db.yellowbrickTag.bulkCreate(data.YellowbrickTag, {
+          ignoreDuplicates: true,
+          validate: true,
+          transaction,
+        });
+      }
+      if (data.YellowbrickTeam) {
+        await db.yellowbrickTeam.bulkCreate(data.YellowbrickTeam, {
+          ignoreDuplicates: true,
+          validate: true,
+          transaction,
+        });
+      }
+      if (data.YellowbrickKml) {
+        for (const kmlObj of data.YellowbrickKml) {
+          await uploadUtil.uploadDataToS3({
+            Bucket: KML_S3_BUCKET,
+            Key: `${kmlObj.id}.kml`,
+            Body: kmlObj.data,
+          });
+        }
+      }
+      if (data.YellowbrickRace) {
+        raceMetadata = await normalizeRace(data, transaction);
+      }
+      await transaction.commit();
+    } catch (error) {
+      console.log(error);
+      await transaction.rollback();
+      errorMessage = databaseErrorHandler(error);
     }
-    if (data.YellowbrickRace) {
-      raceMetadata = await normalizeRace(data, transaction);
+
+    if (raceUrl.length > 0) {
+      if (errorMessage) {
+        await db.yellowbrickFailedUrl.bulkCreate(
+          raceUrl.map((row) => {
+            return {
+              id: uuidv4(),
+              url: row.url,
+              error: errorMessage,
+            };
+          }),
+          {
+            ignoreDuplicates: true,
+            validate: true,
+          },
+        );
+      } else {
+        await db.yellowbrickSuccessfulUrl.bulkCreate(
+          raceUrl.map((row) => {
+            return {
+              id: uuidv4(),
+              url: row.url,
+              original_id: row.original_id,
+            };
+          }),
+          {
+            ignoreDuplicates: true,
+            validate: true,
+          },
+        );
+      }
     }
-    await transaction.commit();
-  } catch (error) {
-    console.log(error);
-    await transaction.rollback();
-    errorMessage = databaseErrorHandler(error);
   }
 
   if (
@@ -97,41 +134,12 @@ const saveYellowbrickData = async (data) => {
     process.env.NODE_ENV !== 'test'
   ) {
     try {
-      await mapYellowBrickToSyrf(data, raceMetadata);
+      if (data.YellowbrickRace?.length) {
+        raceMetadata = await normalizeRace(data);
+        await mapYellowBrickToSyrf(data, raceMetadata);
+      }
     } catch (err) {
       console.log(err);
-    }
-  }
-
-  if (raceUrl.length > 0) {
-    if (errorMessage) {
-      await db.yellowbrickFailedUrl.bulkCreate(
-        raceUrl.map((row) => {
-          return {
-            id: uuidv4(),
-            url: row.url,
-            error: errorMessage,
-          };
-        }),
-        {
-          ignoreDuplicates: true,
-          validate: true,
-        },
-      );
-    } else {
-      await db.yellowbrickSuccessfulUrl.bulkCreate(
-        raceUrl.map((row) => {
-          return {
-            id: uuidv4(),
-            url: row.url,
-            original_id: row.original_id,
-          };
-        }),
-        {
-          ignoreDuplicates: true,
-          validate: true,
-        },
-      );
     }
   }
 
