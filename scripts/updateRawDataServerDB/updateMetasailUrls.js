cbfb6b8cc011675bb6753d07ea8625d7a08ef9b9
Nguyen Manh Tung <nguyenmanhtung848@gmail.com>
2022-01-25 12:16:40 +0700

    SNS-485: update metasail script to update readyAboutRaceMetadata (#140)

--- scripts/updateRawDataServerDB/updateMetasailUrls.js
+++ scripts/updateRawDataServerDB/updateMetasailUrls.js
@@ -2,7 +2,7 @@ const db = require('../../src/models');
 const databaseErrorHandler = require('../../src/utils/databaseErrorHandler');
 const elasticsearch = require('../../src/utils/elasticsearch');
 const Op = db.Sequelize.Op;
-
+const { SOURCE } = require('../../src/constants');
 const updateMetasailUrls = async () => {
   let errorMessage = '';
   const existingRaces = await db.metasailRace.findAll({
@@ -14,9 +14,6 @@ const updateMetasailUrls = async () => {
       },
     },
   });
-  if (existingRaces.length === 0) {
-    return;
-  }
 
   for (const race of existingRaces) {
     const transaction = await db.sequelize.transaction();
@@ -36,6 +33,42 @@ const updateMetasailUrls = async () => {
       await elasticsearch.updateRace(race.id, {
         url,
       });
+      await transaction.commit();
+    } catch (error) {
+      console.log(error.toString());
+      await transaction.rollback();
+      errorMessage = databaseErrorHandler(error);
+    }
+  }
+
+  const existingMetaDatas = await db.readyAboutRaceMetadata.findAll({
+    attributes: ['id', 'url'],
+    raw: true,
+    where: {
+      source: {
+        [Op.eq]: SOURCE.METASAIL,
+      },
+      url: {
+        [Op.notILike]: 'https%',
+      },
+    },
+  });
+
+  for (const data of existingMetaDatas) {
+    const transaction = await db.sequelize.transaction();
+    try {
+      const url = data.url.replace('http', 'https');
+      await db.readyAboutRaceMetadata.update(
+        {
+          url,
+        },
+        {
+          where: {
+            id: data.id,
+          },
+          transaction,
+        },
+      );
       await transaction.commit();
     } catch (error) {
       console.log(error.toString());
