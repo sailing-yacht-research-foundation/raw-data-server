df5c6416928a78d5768828e267e146929936d640
Tung Nguyen <nguyenmanhtung848@gmail.com>
2022-01-31 17:02:37 +0700

    SNS-485: fix transaction problem adding debug info

--- scripts/updateRawDataServerDB/updateMetasailUrls.js
+++ scripts/updateRawDataServerDB/updateMetasailUrls.js
@@ -13,14 +13,17 @@ const { SOURCE } = require('../../src/constants');
   console.log(
     `update existingRaces, there are ${existingRaces.length} races in the database`,
   );
+  let count = 1;
   for (const race of existingRaces) {
+    console.log(
+      `processing race ${race.url}, ${count}/${existingRaces.length}`,
+    );
     let transaction;
     try {
+      transaction = await db.sequelize.transaction();
       const url = race.url.replace('http://', 'https://');
-
       // only update http race
       if (race.url.indexOf('https') === -1) {
-        transaction = await db.sequelize.transaction();
         await db.metasailRace.update(
           {
             url,
@@ -32,17 +35,20 @@ const { SOURCE } = require('../../src/constants');
             transaction,
           },
         );
-        await transaction.commit();
       }
       await elasticsearch.updateRace(race.id, {
         url,
       });
+      await transaction.commit();
     } catch (error) {
+      console.log('Error while updating existing race');
+      console.log(race);
       console.log(error.toString());
       if (transaction) {
         await transaction.rollback();
       }
     }
+    count++;
   }
 
   const existingMetaDatas = await db.readyAboutRaceMetadata.findAll({
@@ -61,8 +67,12 @@ const { SOURCE } = require('../../src/constants');
   console.log(
     `update existingMetaDatas, there are ${existingMetaDatas.length} metadatas in the database`,
   );
+  count = 1;
   for (const data of existingMetaDatas) {
     const transaction = await db.sequelize.transaction();
+    console.log(
+      `process ${count}/${existingMetaDatas.length} metadata, url = ${data.url}`,
+    );
     try {
       const url = data.url.replace('http', 'https');
       await db.readyAboutRaceMetadata.update(
@@ -78,6 +88,8 @@ const { SOURCE } = require('../../src/constants');
       );
       await transaction.commit();
     } catch (error) {
+      console.log('Error while updating existing metadata');
+      console.log(data);
       console.log(error.toString());
       await transaction.rollback();
     }
