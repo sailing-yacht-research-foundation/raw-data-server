b1f1832907e2fc607cea01aaff0b80c91e3b4032
Tung Nguyen <nguyenmanhtung848@gmail.com>
2022-02-18 15:59:28 +0700

    SNS-656: add logic to index future race for geovoile

--- src/services/saveGeovoileData.js
+++ src/services/saveGeovoileData.js
@@ -1,11 +1,14 @@
 const { v4: uuidv4 } = require('uuid');
 
-const { SAVE_DB_POSITION_CHUNK_COUNT } = require('../constants');
+const { SAVE_DB_POSITION_CHUNK_COUNT, SOURCE } = require('../constants');
 const db = require('../models');
 const databaseErrorHandler = require('../utils/databaseErrorHandler');
 const { triggerWeatherSlicer } = require('./weatherSlicerUtil');
 const { normalizeGeovoile } = require('./normalization/normalizeGeovoile');
 const mapGeovoileToSyrf = require('./mappingsToSyrfDB/mapGeovoileToSyrf');
+const elasticsearch = require('../utils/elasticsearch');
+const { getTrackerLogoUrl } = require('./s3Util');
+const { createTurfPoint, getCountryAndCity } = require('../utils/gisUtils');
 
 const saveSuccessfulUrl = async (original_id, url) => {
   await db.geovoileSuccessfulUrl.create({ url, original_id, id: uuidv4() });
@@ -131,19 +134,90 @@ const saveGeovoileData = async (data) => {
     process.env.NODE_ENV !== 'test'
   ) {
     try {
-      raceMetadata = await normalizeGeovoile(data);
-      await mapGeovoileToSyrf(data, raceMetadata);
+      const { geovoileRace } = data;
+      const isUnfinished =
+        geovoileRace.eventState !== 'FINISH' &&
+        geovoileRace.raceState !== 'FINISH';
+      if (isUnfinished) {
+        console.log(
+          `Future race detected for race scrapedUrl = ${geovoileRace.scrapedUrl}`,
+        );
+        try {
+          // The deletion of previous elastic search is on a different endpoint and will be triggered by the tracker-scraper
+          await _indexUnfinishedRaceToES(geovoileRace, data);
+        } catch (err) {
+          console.log(
+            `Failed indexing unfinished race scrapedUrl ${geovoileRace.scrapedUrl}`,
+            err,
+          );
+        }
+      } else {
+        raceMetadata = await normalizeGeovoile(data);
+        await mapGeovoileToSyrf(data, raceMetadata);
+      }
     } catch (err) {
       console.log(err);
       errorMessage = databaseErrorHandler(err);
     }
   }
 
-  if (!errorMessage) {
+  if (!errorMessage && raceMetadata) {
     await triggerWeatherSlicer(raceMetadata);
   }
 
   return errorMessage;
+};
+
+const _indexUnfinishedRaceToES = async (race, data) => {
+  const startTimeMs = race.startTime * 1000;
+  const startDate = new Date(startTimeMs);
+  const startMark = data.marks?.find(
+    (mark) => mark?.type.toLowerCase()?.indexOf('start') >= 0,
+  );
+  const startGate = data.courseGates?.find(
+    (gate) => gate.properties?.name?.toLowerCase()?.indexOf('start') >= 0,
+  );
+  let startPoint;
+  if (startMark) {
+    startPoint = createTurfPoint(startMark.lat, startMark.lon);
+  } else if (!startMark && startGate) {
+    const coordinate = startGate.coordinates[0];
+    startPoint = createTurfPoint(
+      coordinate.position[1],
+      coordinate.position[0],
+    );
+  }
+
+  const body = {
+    id: race.id,
+    name: race.name,
+    source: SOURCE.GEOVOILE,
+    url: race.scrapedUrl,
+    start_year: startDate.getUTCFullYear(),
+    start_month: startDate.getUTCMonth() + 1,
+    start_day: startDate.getUTCDate(),
+    approx_start_time_ms: startTimeMs,
+    approx_end_time_ms: (race.endTime || 0) * 1000,
+    open_graph_image: getTrackerLogoUrl(SOURCE.GEOVOILE),
+    is_unfinished: true, // only attribute for unfinished races
+    scraped_original_id: race.scrapedUrl,
+  };
+
+  if (startPoint) {
+    body.approx_start_point = startPoint.geometry;
+    const { countryName, cityName } = await getCountryAndCity({
+      lon: startPoint.geometry.coordinates[0],
+      lat: startPoint.geometry.coordinates[1],
+    });
+
+    if (countryName) {
+      body.start_country = countryName;
+    }
+    if (cityName) {
+      body.start_city = cityName;
+    }
+  }
+  await elasticsearch.indexRace(race.id, body);
 };
 
 module.exports = saveGeovoileData;
