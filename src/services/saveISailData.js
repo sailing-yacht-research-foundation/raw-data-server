7fe602d46704d8b847422dd9cfbf3f19d25e0a59
Jackson Tan <jackson.tan.s@gmail.com>
2022-03-24 12:57:07 +0800

    SNS-657 - Include isail unfinished races and index in elastic search (#187)

--- src/services/saveISailData.js
+++ src/services/saveISailData.js
@@ -1,11 +1,18 @@
 const { v4: uuidv4 } = require('uuid');
 
-const { SAVE_DB_POSITION_CHUNK_COUNT } = require('../constants');
+const { SAVE_DB_POSITION_CHUNK_COUNT, SOURCE } = require('../constants');
 const db = require('../models');
 const databaseErrorHandler = require('../utils/databaseErrorHandler');
 const { normalizeRace } = require('./normalization/normalizeISail');
 const mapAndSave = require('./mappingsToSyrfDB/mapIsailToSyrf');
 const { triggerWeatherSlicer } = require('./weatherSlicerUtil');
+const elasticsearch = require('../utils/elasticsearch');
+const {
+  generateMetadataName,
+  findCenter,
+  getCountryAndCity,
+} = require('../utils/gisUtils');
+const { getTrackerLogoUrl } = require('./s3Util');
 
 const saveISailData = async (data) => {
   let errorMessage = '';
@@ -158,14 +165,41 @@ const saveISailData = async (data) => {
     process.env.ENABLE_MAIN_DB_SAVE_ISAIL === 'true' &&
     process.env.NODE_ENV !== 'test'
   ) {
-    try {
-      if (data.iSailRace?.length) {
-        raceMetadatas = await normalizeRace(data);
-        await mapAndSave(data, raceMetadatas);
+    const finishedRaces = [];
+    for (const race of data.iSailRace) {
+      const now = Date.now();
+      const startTime = race.start * 1000;
+      const endTime = race.stop * 1000;
+      const isUnfinished = startTime > now || endTime > now;
+      if (isUnfinished) {
+        console.log(
+          `Future race detected for race original id ${race.original_id}`,
+        );
+        try {
+          // The deletion of previous elastic search is on a different endpoint and will be triggered by the tracker-scraper
+          await _indexUnfinishedRaceToES(race, data);
+        } catch (err) {
+          console.log(
+            `Failed indexing unfinished race original id ${race.original_id}`,
+            err,
+          );
+        }
+      } else {
+        finishedRaces.push(race);
       }
-    } catch (err) {
-      console.log(err);
-      errorMessage = databaseErrorHandler(err);
+    }
+    data.iSailRace = finishedRaces;
+
+    if (data.iSailRace.length) {
+      try {
+        raceMetadatas = await normalizeRace(data);
+        if (raceMetadatas?.length) {
+          await mapAndSave(data, raceMetadatas);
+        }
+      } catch (err) {
+        console.log(err);
+        errorMessage = databaseErrorHandler(err);
+      }
     }
   }
 
@@ -175,6 +209,63 @@ const saveISailData = async (data) => {
     }
   }
   return errorMessage;
+};
+
+const _indexUnfinishedRaceToES = async (race, data) => {
+  const event = data.iSailEvent[0];
+  const startTime = race.start * 1000;
+  const endTime = race.stop * 1000;
+  const startDate = new Date(startTime);
+  const name = generateMetadataName(event.name, race.name, startTime);
+  const eventStartLines = data.iSailStartline;
+  const raceStartLines = eventStartLines?.filter(
+    (s) => s.original_race_id === race.original_id,
+  );
+  const startLine =
+    raceStartLines?.find((sl) => sl.name === 'start') || raceStartLines?.[0];
+  let startPoint;
+  if (startLine) {
+    startPoint = findCenter(
+      startLine.lat_1,
+      startLine.lon_1,
+      startLine.lat_2,
+      startLine.lon_2,
+    );
+  }
+
+  const body = {
+    id: race.id,
+    name,
+    event: event.id,
+    event_name: event.name,
+    source: SOURCE.ISAIL,
+    url: race.url,
+    start_year: startDate.getUTCFullYear(),
+    start_month: startDate.getUTCMonth() + 1,
+    start_day: startDate.getUTCDate(),
+    approx_start_time_ms: startTime,
+    approx_end_time_ms: endTime,
+    open_graph_image: getTrackerLogoUrl(SOURCE.ISAIL), // use tracker logo for unfinished races
+    is_unfinished: true, // only attribute for unfinished races
+    scraped_original_id: race.original_id.toString(), // Used to check if race has been indexed in es. Convert to string for other scraper uses uid instead of int
+  };
+
+  if (startPoint) {
+    body.approx_start_point = startPoint.geometry;
+    const { countryName, cityName } = await getCountryAndCity({
+      lon: startPoint.geometry.coordinates[0],
+      lat: startPoint.geometry.coordinates[1],
+    });
+
+    if (countryName) {
+      body.start_country = countryName;
+    }
+    if (cityName) {
+      body.start_city = cityName;
+    }
+  }
+
+  await elasticsearch.indexRace(race.id, body);
 };
 
 module.exports = saveISailData;
