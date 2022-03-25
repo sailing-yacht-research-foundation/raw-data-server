1f162fdbab2d1edf75470cdbc9672ba91c6927c7
Jackson Tan <jackson.tan.s@gmail.com>
2022-03-25 23:30:38 +0800

    SNS-661 - Include RaceQs unfinished races on elastic search

--- src/services/saveRaceQsData.js
+++ src/services/saveRaceQsData.js
@@ -1,11 +1,18 @@
 const { v4: uuidv4 } = require('uuid');
 
-const { SAVE_DB_POSITION_CHUNK_COUNT } = require('../constants');
+const { SAVE_DB_POSITION_CHUNK_COUNT, SOURCE } = require('../constants');
 const db = require('../models');
 const databaseErrorHandler = require('../utils/databaseErrorHandler');
 const { normalizeRace } = require('./normalization/normalizeRaceQs');
 const { triggerWeatherSlicer } = require('./weatherSlicerUtil');
 const mapRaceQsToSyrf = require('../services/mappingsToSyrfDB/mapRaceQsToSyrf');
+const elasticsearch = require('../utils/elasticsearch');
+const {
+  generateMetadataName,
+  createTurfPoint,
+  getCountryAndCity,
+} = require('../utils/gisUtils');
+const { getTrackerLogoUrl } = require('./s3Util');
 
 const saveRaceQsData = async (data) => {
   let errorMessage = '';
@@ -147,21 +154,100 @@ const saveRaceQsData = async (data) => {
     process.env.ENABLE_MAIN_DB_SAVE_RACEQS === 'true' &&
     process.env.NODE_ENV !== 'test'
   ) {
-    try {
-      raceMetadatas = await normalizeRace(data);
-      await mapRaceQsToSyrf(data, raceMetadatas);
-    } catch (err) {
-      console.log(err);
-      errorMessage = databaseErrorHandler(err);
+    const finishedRaces = [];
+    for (const race of data.RaceQsEvent) {
+      const now = Date.now();
+      const startTime = +race.from;
+      const endTime = +race.till;
+      const isUnfinished = startTime > now || !endTime || endTime > now;
+      if (isUnfinished) {
+        console.log(
+          `Future race detected for race original id ${race.original_id}`,
+        );
+        try {
+          // The deletion of previous elastic search is on a different endpoint and will be triggered by the tracker-scraper
+          await _indexUnfinishedRaceToES(race, data);
+        } catch (err) {
+          console.log(
+            `Failed indexing unfinished race original id ${race.original_id}`,
+            err,
+          );
+        }
+      } else {
+        finishedRaces.push(race);
+      }
+    }
+    data.RaceQsEvent = finishedRaces;
+
+    if (data.RaceQsEvent.length) {
+      try {
+        raceMetadatas = await normalizeRace(data);
+        await mapRaceQsToSyrf(data, raceMetadatas);
+      } catch (err) {
+        console.log(err);
+        errorMessage = databaseErrorHandler(err);
+      }
     }
   }
 
   if (raceMetadatas && !errorMessage) {
-    for (raceMetadata of raceMetadatas) {
+    for (const raceMetadata of raceMetadatas) {
       await triggerWeatherSlicer(raceMetadata);
     }
   }
   return errorMessage;
+};
+
+const _indexUnfinishedRaceToES = async (race, data) => {
+  const regatta = data.RaceQsRegatta[0];
+  const waypoints = data.RaceQsWaypoint;
+  const startTimeMs = +race.from;
+  const endTimeMs = +race.till;
+  const startDate = new Date(startTimeMs);
+  const name = generateMetadataName(regatta.name, race.name, startTimeMs);
+  const startWaypoint =
+    waypoints?.find((wp) => wp.type === 'Start') || waypoints?.[0];
+  let startPoint;
+
+  if (startWaypoint && startWaypoint.lat && startWaypoint.lon) {
+    startPoint = createTurfPoint(startWaypoint.lat, startWaypoint.lon);
+  } else if (race.lat1 && race.lon1) {
+    startPoint = createTurfPoint(race.lat1, race.lon1);
+  }
+
+  const body = {
+    id: race.id,
+    name,
+    event: regatta.id,
+    event_name: regatta.name,
+    source: SOURCE.RACEQS,
+    url: race.url,
+    start_year: startDate.getUTCFullYear(),
+    start_month: startDate.getUTCMonth() + 1,
+    start_day: startDate.getUTCDate(),
+    approx_start_time_ms: startTimeMs,
+    approx_end_time_ms: endTimeMs,
+    open_graph_image: getTrackerLogoUrl(SOURCE.RACEQS), // use tracker logo for unfinished races
+    is_unfinished: true, // only attribute for unfinished races
+    scraped_original_id: race.original_id.toString(), // Used to check if race has been indexed in es. Convert to string for other scraper uses uid instead of int
+  };
+
+  if (startPoint) {
+    body.approx_start_point = startPoint.geometry;
+    const { countryName, cityName } = await getCountryAndCity({
+      lon: startPoint.geometry.coordinates[0],
+      lat: startPoint.geometry.coordinates[1],
+    });
+
+    if (countryName) {
+      body.start_country = countryName;
+    }
+    if (cityName) {
+      body.start_city = cityName;
+    }
+  }
+
+  await elasticsearch.indexRace(race.id, body);
 };
 
 module.exports = saveRaceQsData;
