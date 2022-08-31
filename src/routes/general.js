5db5c5a21cc29de11b6571004edaa1ed4d9146b8
AlvinJK <ajk.alvin@gmail.com>
2022-08-31 18:40:38 +0700

    Add modelVersion

--- src/routes/general.js
+++ src/routes/general.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 
+const modelVersion = require('../syrf-schema/package.json').version;
+
 var router = express.Router();
 
 router.get('/healthcheck', cors(), function (req, res) {
   res.json({
     message: 'ok',
     version: process.env.npm_package_version,
+    modelVersion,
     uptime: process.uptime(),
   });
 });
